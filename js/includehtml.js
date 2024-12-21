48604f2066438c3aab3e2083444767c1f185e8a1
kcm5221 <135325219+kcm5221@users.noreply.github.com>
2024-12-21 18:35:18 +0900

    category
    .

--- js/includehtml.js
+++ js/includehtml.js
@@ -13,4 +13,18 @@ document.addEventListener('DOMContentLoaded', function () {
             document.getElementById('footer').innerHTML = data;
         })
         .catch(error => console.error('footer.html load fail', error));
+    //nav 삽입
+    fetch('/nav.html')
+        .then(Response => Response.text())
+        .then(data => {
+            document.getElementById('nav').innerHTML = data;
+        })
+        .catch(error => console.error('nav.html load fail', error));
+    //aside 삽입
+    fetch('/aside.html')
+        .then(Response => Response.text())
+        .then(data => {
+            document.getElementById('aside').innerHTML = data;
+        })
+        .catch(error => console.error('aside.html load fail', error));
 });
